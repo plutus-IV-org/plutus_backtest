7063c901dbfa9e060e5acd0f7d3bccc7219e55b4
witmul <83119547+witmul@users.noreply.github.com>
2021-11-15 19:27:07 +0100

    adding initial files

---
+++ backt_code/test_file.py
@@ -0,0 +1,110 @@
+import pandas as pd
+import yfinance as yf
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+class backtest:
+    def __init__(self, comp, b_day, s_day):
+        self.comp = comp
+        self.b_day = b_day
+        self.s_day = s_day
+
+
+    def company_list(self):
+        df = pd.DataFrame({"company": self.comp,
+                           "buy day": self.b_day,
+                           "sell day": self.s_day})
+        self.company_list = df
+
+
+    def pricing(self):
+        backtest.company_list(self)
+        symbols = self.company_list['company'].values.tolist()
+        data = yf.download(symbols, start=min(self.b_day), end=max(self.s_day))
+        price = data[["Open", 'Adj Close']]
+        price.index = price.index.strftime('%Y-%m-%d')
+        self.price = price
+
+
+    def consolidated(self):
+        backtest.pricing(self)
+        df_1 = self.company_list
+        df_2 = self.price
+
+        buy_list = []
+        sell_list = []
+
+        for com, b_d, s_d in zip (df_1["company"], df_1["buy day"], df_1["sell day"]):
+            buy_list.append(df_2.loc[b_d, df_2.columns.get_level_values(1) == com]["Open"][0])
+            sell_list.append(df_2.loc[b_d, df_2.columns.get_level_values(1) == com]["Adj Close"][0])
+
+        df_1["buy price"] = buy_list
+        df_1["sell price"] = sell_list
+        self.table = df_1
+        return self.table
+
+
+    def consolidated_detailed(self):
+        backtest.consolidated(self)
+        initial_df = pd.DataFrame()
+
+        for com, b_d, s_d in zip(self.table["company"], self.table["buy day"], self.table["sell day"]):
+            data = yf.download(com, start=b_d, end=s_d)
+            data["Ticker"] = com
+            initial_df = pd.concat([initial_df, data])
+
+        df = initial_df[["Ticker", "Adj Close"]]
+        df.columns = ['ticker', 'price']
+        df1 = df.pivot_table(index=df.index, columns='ticker', values=['price'])
+
+        df1 = df1.replace([np.inf, -np.inf], np.nan)
+        df1 = df1.fillna(method = "ffill")
+        df1 = df1.fillna(0)
+        df1 = df1.apply(pd.to_numeric)
+
+        df_daily_returns = df1.pct_change()
+
+        df_daily_returns = df_daily_returns.replace([np.inf, -np.inf], np.nan)
+        df_daily_returns = df_daily_returns.fillna(0)
+        df_daily_returns = df_daily_returns.apply(pd.to_numeric)
+
+        df_daily_returns["Total"] = df_daily_returns.sum(axis=1)
+
+
+        #df_daily_returns = df_daily_returns.replace([np.inf, -np.inf], np.nan)
+        #df_daily_returns = df_daily_returns.fillna(0)
+
+        #df_daily_returns = df_daily_returns.apply(pd.to_numeric) * 100
+
+        df_daily_returns = df_daily_returns.iloc[1: , :]
+
+        df_daily_returns["Cumulative return"] = (1 + df_daily_returns["Total"]).cumprod()
+
+
+        #cum_prod = (1+df_daily_returns).cumprod()
+        self.detailed_return = df_daily_returns
+        return self.detailed_return
+
+
+    def ploting (self):
+        backtest.consolidated_detailed(self)
+
+        fig2 = plt.figure(figsize=(15, 7))
+        ax2 = fig2.add_subplot(1, 1, 1)
+        self.detailed_return["Cumulative return"].plot(ax=ax2)
+        ax2.set_xlabel('Date')
+        ax2.set_ylabel('Cumulative return')
+        ax2.set_title('Portfolio Cumulative Returns')
+        plt.show()
+
+
+
+bt = backtest(["FDX", "APA"],
+              ["2017-01-03", "2021-03-02"],
+              ["2021-02-12", "2021-05-12"])
+
+
+print(bt.consolidated_detailed())
+print(bt.ploting())
---
+++ backt_code/unit test.py
@@ -0,0 +1,11 @@
+from backt_code.main import backtest
+
+bt = backtest(["FDX", "APA", "MSFT"],
+              ["2019-01-15", "2016-03-02", "2016-10-21"],
+              ["2021-02-11", "2020-06-24", "2020-08-20"]) # max sell date is shown as max value -1 day
+
+
+
+print(bt.consolidated())
+print(bt.consolidated_detailed())
+
