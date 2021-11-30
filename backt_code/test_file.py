474cec42a86b231f2157d4099ad310604252c6c6
IlliaBoloto <83161286+IlliaBoloto@users.noreply.github.com>
2021-11-30 11:25:41 +0100

    30/11/21 Added sl tp signals + fixed weights

--- backt_code/test_file.py
+++ backt_code/test_file.py
@@ -7,16 +7,24 @@ from datetime import datetime, timedelta
 
 
 class backtest:
-    def __init__(self, comp, b_day, s_day, weights_factor= None):
+    def __init__(self, comp, b_day, s_day, weights_factor= None , take_profit=None, stop_loss=None):
         self.comp = comp
         self.b_day = b_day
         self.s_day = s_day
         self.w_factor = weights_factor if weights_factor is not None else np.ones(len(comp))
+        self.tp = take_profit if take_profit is not None else 100 * np.ones(len(comp))
+        self.sl = stop_loss if stop_loss is not None else np.zeros(len(comp))
 
     def company_list(self):
-        df = pd.DataFrame({"company": self.comp,
-                           "buy day": self.b_day,
-                           "sell day": self.s_day})
+        df = pd.DataFrame({"company": self.comp,"start day": self.b_day,"end day": self.s_day,"weights factor": self.w_factor,
+                           "take profit": self.tp,"stop loss": self.sl})
+        df = df.set_index(df['company'])
+        for x in df.index:
+            if df.loc[x, "weights factor"] < 0:
+                a = df.loc[x, "stop loss"]
+                b = df.loc[x, "take profit"]
+                df.loc[x,"take profit"] = (1-a) + 1
+                df.loc[x,"stop loss"] = 1 - (b-1)
         self.company_list = df
 
 # FOR DETAILED VIEW Adding one day to first buy and last sell dates
@@ -32,7 +40,7 @@ class backtest:
         df_1 = self.company_list
         initial_df = pd.DataFrame()
 
-        for com, b_d, s_d in zip(df_1["company"], df_1["buy day"], df_1["sell day"]):
+        for com, b_d, s_d in zip(df_1["company"], df_1["start day"], df_1["end day"]):
             data = yf.download(com, start=backtest.date_plus_one(self, b_d), end=backtest.date_plus_one(self, s_d))
             data["Ticker"] = com
             initial_df = pd.concat([initial_df, data])
@@ -69,15 +77,48 @@ class backtest:
         aux = aux.apply(pd.to_numeric)
         self.auxiliar_df = aux
         self.detailed_return = dc
+
         return self.detailed_return
 
 
     def portfolio_construction(self):
-        binar_weights = self.auxiliar_df / self.auxiliar_df
-        binar_weights.fillna(value=0, inplace=True)
+        binary_weights = self.auxiliar_df / self.auxiliar_df
+        binary_weights.fillna(value=0, inplace=True)
         fac_summing = np.sum(abs(np.array(self.w_factor)))
-        dist = np.array(self.w_factor)/fac_summing
-        weights_df = binar_weights * dist
+        dist = np.array(self.w_factor) / fac_summing
+        dist_df = pd.DataFrame(index= self.comp, data = self.w_factor).T
+        weights_df = binary_weights * dist
+        for z in weights_df.index:
+            if abs(weights_df.loc[z]).sum()!=1:
+                act = weights_df.loc[z][weights_df.loc[z]!=0]
+                act_df = dist_df[act.index]
+                new_dist =np.array(act_df)/np.sum(abs(np.array(act_df)))
+                new_dist_frame = pd.DataFrame(columns = act.index, data = new_dist).T
+                for i in new_dist_frame.index:
+                    weights_df.loc[z,i] =float(new_dist_frame.loc[i].values)
+
+
+        accu = (self.detailed_return + 1).cumprod()
+        for x in accu.columns:
+            q1 = accu[x]
+            for y in q1:
+                if y> self.company_list.loc[x, 'take profit']:
+                    q1.iloc[q1.values.tolist().index(y)+1:] =0
+                if y< self.company_list.loc[x, 'stop loss']:
+                    q1.iloc[q1.values.tolist().index(y) + 1:] = 0
+        aux_table_2 = accu* binary_weights
+        new_binary_weights = aux_table_2/aux_table_2
+        new_binary_weights.fillna(value=0, inplace=True)
+        weights_df = new_binary_weights * dist
+        for z in weights_df.index:
+            if abs(weights_df.loc[z]).sum() != 1:
+                act = weights_df.loc[z][weights_df.loc[z] != 0]
+                act_df = dist_df[act.index]
+                new_dist = np.array(act_df) / np.sum(abs(np.array(act_df)))
+                new_dist_frame = pd.DataFrame(columns=act.index, data=new_dist).T
+                for i in new_dist_frame.index:
+                    weights_df.loc[z, i] = float(new_dist_frame.loc[i].values)
+
         port_performance = weights_df * self.detailed_return
         port_performance['Sum'] = port_performance.sum(axis=1)
         port_performance['Sum'] = port_performance['Sum'] + 1
@@ -122,4 +163,6 @@ class backtest:
         print(f'There is 95% confidence that we will not lose more than {round(100*VaR_95,2)} % of your portfolio in a given {self.trade_length} period.')
         print(f'There is 99% confidence that we will not lose more than {round(100*VaR_99,2)} % of your portfolio in a given {self.trade_length} period.')
         print(f'Expected loss that occur beyond the shortfall is {round(CVaR,4)}.')
+    def puzzle_assembly(self, dic):
+        return
 
--- backt_code/testing code.py
+++ backt_code/testing code.py
@@ -1,12 +1,15 @@
 from backt_code.test_file import backtest
 
+
+
 bt = backtest(["ATVI", "DXC", "APA"],
               ["2021-10-15", "2021-10-01", "2021-09-15"],
-              ["2021-11-17", "2021-11-17", "2021-11-17"])
+              ["2021-11-17", "2021-11-17", "2021-11-17"],take_profit=[1.2, 1.05, 1.5], stop_loss=[0.9, 0.7, 0.8])
 
-
-
+#weights_factor=[1,-3, 10], take_profit=[1.2, 1.05, 1.5], stop_loss=[0.9, 0.7, 0.8]
+print(bt.company_list())
 print(bt.consolidated_table_detailed())
+
 print(bt.portfolio_construction())
 print(bt.general_statistic())
 print(bt.Var_and_CVaR())
