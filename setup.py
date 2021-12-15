5788276ffd26ed479e5fd7a8b70c5296c8a245ff
witmul <83119547+witmul@users.noreply.github.com>
2021-12-15 18:49:43 +0100

    including pypi files / test files removed

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup , find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='backt',
     version='0.0.1',
+    author="Example Author",
+    author_email="author@example.com",
     packages= find_packages(),
+    url="https://github.com/witmul/backt/tree/main",
     install_requires=[
         'pandas',
         'yfinance',
