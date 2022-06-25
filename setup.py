ab4e65cfe7c5ca0b72033360fbfee8c0e7eb789b
witmul <83119547+witmul@users.noreply.github.com>
2022-06-25 12:51:02 +0200

    setup for release

--- setup.py
+++ setup.py
@@ -4,52 +4,52 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='L_O_P_A_T_A',
+    name='plutus_backtest',
 
-    version='2.0.8',
+    version='0.3.0.0',
 
-    #description="plutus is a python package \
-    #for backtesting investment decisions using Python 3.6 and above.",
+    description="plutus is a python package \
+    for backtesting investment decisions using Python 3.6 and above.",
 
     long_description_content_type='text/markdown',
 
     license="MIT",
 
-    #keywords="backtest python stock portfolio trade daytrading finance downside risk management accumulation return profit loss optimization asset security currency crypto futures derivatives test",
+    keywords="backtest python stock portfolio trade daytrading finance downside risk management accumulation return profit loss optimization asset security currency crypto futures derivatives test",
 
-    #author="IlliaBoloto",
+    author="IlliaBoloto",
 
-    #author_email="ils.boloto96@gmail.com",
+    author_email="ils.boloto96@gmail.com",
 
-    #long_description=long_description,
+    long_description=long_description,
 
 # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-#     classifiers=[
-#         # How mature is this project? Common values are
-#         #   2 - Pre-Alpha
-#         #   3 - Alpha
-#         #   4 - Beta
-#         #   5 - Production/Stable
-#         'Development Status :: 4 - Beta',
-# # Indicate who your project is intended for
-#         'Intended Audience :: Developers',
-#         'Intended Audience :: Financial and Insurance Industry',
-#
-#         # Indicate which Topics are covered by the package
-#         'Topic :: Software Development',
-#         'Topic :: Office/Business :: Financial',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   2 - Pre-Alpha
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+# Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+
+        # Indicate which Topics are covered by the package
+        'Topic :: Software Development',
+        'Topic :: Office/Business :: Financial',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-    #     "Programming Language :: Python :: 3.6",
-    #     "Programming Language :: Python :: 3.7",
-    #     "Programming Language :: Python :: 3.8",
-    #     'Programming Language :: Python :: 3.9',
-    # ],
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        'Programming Language :: Python :: 3.9',
+    ],
 
     packages=find_packages(include=['plutus', 'plutus.*']),
 
-    #url="https://github.com/witmul/plutus_backtest",
+    url="https://github.com/witmul/plutus_backtest",
 
     python_requires=">=3.6.0",
 
